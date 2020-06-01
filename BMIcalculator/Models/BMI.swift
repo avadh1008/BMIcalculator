23cc14c8b0299eff0a7c548bf214e6b14eeeb5fd
Avadh Mewada <avadh.mewada@aimsinfosoft.com>
2020-06-01 20:14:06 +0530

    Initial Commit

---
+++ BMIcalculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMIcalculator
+//
+//  Created by Avadh Mewada on 6/1/20.
+//  Copyright © 2020 Avadh Mewada. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
